b73ebd037382d0e10aba2e9af5f3c6c3ded63e17
AlexandrTolstov <alextav_85@mail.ru>
2019-03-19 11:20:25 +0600

    1

--- InterfaceAbstractClass/Program.cs
+++ InterfaceAbstractClass/Program.cs
@@ -10,6 +10,53 @@ namespace InterfaceAbstractClass
     {
         static void Main(string[] args)
         {
+            List<IMovable> transport = new List<IMovable>();
+
+            transport.Add(new Car());
+            transport.Add(new Bus());
+            transport.Add(new Hourse());
+            transport.Add(new Aircraft());
+
+            foreach (var item in transport)
+            {
+                item.Move();
+            }
+        }
+    }
+    public interface IMovable
+    {
+        void Move();
+    }
+    public abstract class Vehicle
+    {
+    }
+
+    public class Car : Vehicle, IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Машина едет");
+        }
+    }
+    public class Bus : Vehicle, IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Автобус едет");
+        }
+    }
+    public class Hourse : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Лошадь скачет");
+        }
+    }
+    public class Aircraft : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Самолет летит");
         }
     }
 }
